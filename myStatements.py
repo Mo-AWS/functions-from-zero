6330b89146409ce59538690c813733cfac57e567
Mohammad <mhsrwcc@gmail.com>
2026-02-24 18:46:23 +0330

    add py files

---
+++ myStatements.py
@@ -0,0 +1,9 @@
+my_dict = {"drink": "coffee", "milk": "whole", "sugar": "brown"}
+my_list = ["apple", "cherry", "banana"]
+
+
+for key, value in my_dict.items():
+    print(f"My favurite thing to eat is: {value}", end="\n\n")
+
+for items in my_list:
+    print(f"This is my items: {items}")
