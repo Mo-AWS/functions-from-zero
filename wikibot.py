57be539661e447187c8e3e96e17cbf1530fb02a1
Mohammad <mhsrwcc@gmail.com>
2026-02-26 14:26:07 +0330

    added name and length arguments

--- wikibot.py
+++ wikibot.py
@@ -4,7 +4,7 @@ from mylib.bot import scrape
 @click.command()
 @click.option('--name', help='The name of the topic to search for.')
 @click.option('--length', default=2, help='The number of sentences to return.')
-def cli(name, length):
+def cli(name='Wikipedia', length=2):
     result = scrape(name, length=length)
     click.echo(click.style(result, bg='white', fg='green', bold=True))
 
